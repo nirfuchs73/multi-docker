13e5c9958499af1ee193b582dead3b9422aceb91
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-12-05 19:42:53 +0200

    Test new image for multi-client

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. version 2
         </p>
           <a
             className="App-link"
